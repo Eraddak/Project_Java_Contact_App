ef56e8f2f9c161c95a8a232ee56cfbaf66c930b0
François cresson <francois.cresson@isen.yncrea.fr>
2020-02-03 09:47:37 +0100

    First push
    First implementation of following files :
        - Person.java
        - Contacts.java
        - ContactApp.java

---
+++ src/main/java/Contacts.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Contacts {
+
+    private ArrayList<Person> contacts = new ArrayList<Person>();
+
+    public Contacts(ArrayList<Person> contacts) {
+        this.contacts = contacts;
+    }
+
+    public void addPerson(Person... persons){
+        if(persons == null || persons.length == 0) return;
+        this.contacts.addAll(Arrays.asList(persons));
+    }
+
+    public void deletePerson(Person... persons){
+        if(persons == null || persons.length == 0) return;
+        this.contacts.removeAll(Arrays.asList(persons));
+    }
+
+    public void changeLastName(Person person, String newLastName){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setLastName(newLastName);
+    }
+    public void changeLastName(int index, String newLastName){
+        this.contacts.get(index).setLastName(newLastName);
+    }
+
+    public void changeFirstName(Person person, String newFirstName){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setFirstName(newFirstName);
+    }
+    public void changeFirstName(int index, String newFirstName){
+        this.contacts.get(index).setFirstName(newFirstName);
+    }
+
+    public void changeNickName(Person person, String newNickName){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setNickName(newNickName);
+    }
+    public void changeNickName(int index, String newNickName){
+        this.contacts.get(index).setNickName(newNickName);
+    }
+
+    public void changePhoneNumber(Person person, String newPhoneNumber){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setPhoneNumber(newPhoneNumber);
+    }
+    public void changePhoneNumber(int index, String newPhoneNumber){
+        this.contacts.get(index).setPhoneNumber(newPhoneNumber);
+    }
+
+    public void changeAddress(Person person, String newAddress){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setAddress(newAddress);
+    }
+    public void changeAddress(int index, String newAddress){
+        this.contacts.get(index).setAddress(newAddress);
+    }
+
+    public void changeEmailAddress(Person person, String newEmailAddress){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setEmailAddress(newEmailAddress);
+    }
+    public void changeEmailAddress(int index, String newEmailAddress){
+        this.contacts.get(index).setEmailAddress(newEmailAddress);
+    }
+
+    public void changeBirthDate(Person person, Date newDate){
+        int index = this.contacts.indexOf(person);
+        this.contacts.get(index).setBirthDate(newDate);
+    }
+    public void changeBirthDate(int index, Date newDate){
+        this.contacts.get(index).setBirthDate(newDate);
+    }
+
+    // Getters
+    public ArrayList<Person> getContacts() {
+        return contacts;
+    }
+
+    // Setters
+    public void setContacts(ArrayList<Person> contacts) {
+        this.contacts = contacts;
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,81 @@
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Person {
+
+    private static final AtomicLong ID_FACTORY = new AtomicLong();
+    private final long id;
+    private String lastName;
+    private String firstName;
+    private String nickName = null;
+    private String phoneNumber = null;
+    private String address = null;
+    private String emailAddress = null;
+    private Date birthDate = null;
+
+
+    public Person() {
+        this.id = ID_FACTORY.getAndIncrement();
+    }
+
+    public Person(String lastName, String firstName, String nickName, String phoneNumber, String address, String emailAddress, Date birthDate) {
+        this.id = ID_FACTORY.getAndIncrement();
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.emailAddress = emailAddress;
+        this.birthDate = birthDate;
+    }
+
+    // Getters
+    public long getId() {
+        return id;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getNickName() {
+        return nickName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    // Setters
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
